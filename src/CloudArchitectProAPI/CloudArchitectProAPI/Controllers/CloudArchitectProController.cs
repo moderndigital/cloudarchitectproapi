1efaaef3df615137333968e9e5fd4e81526d6291
David Mead <david@moderndigital.com>
2021-11-28 17:54:07 -0500

    Added CloudArchitectProAPIController ... and Test Controller.
    get /api/cloudarchitectpro works locally on IIS Express

---
+++ src/CloudArchitectProAPI/CloudArchitectProAPI/Controllers/CloudArchitectProController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+
+namespace CloudArchitectProAPI.Controllers
+{
+    public class CloudArchitectProController : ApiController
+    {
+        // GET api/cloudarchitectpro
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
